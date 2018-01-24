d642dd494096f6fbef163b60de38bb3035af3024
Eric Torunski <etorunski@gmail.com>
2018-01-23 21:08:34 -0500

    set android version to 22

--- app/src/main/java/com/example/torunse/inclassexamples_18w/MainActivity.java
+++ app/src/main/java/com/example/torunse/inclassexamples_18w/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.torunse.inclassexamples_18w;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView)findViewById(R.id.textview);
+        Button btn1 = (Button)findViewById(R.id.button1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    tv.setText("You clicked me");
+                }
+                catch(NullPointerException npe)
+                {
+
+                }
+            }
+        });
     }
 }
