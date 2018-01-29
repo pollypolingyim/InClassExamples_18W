c0995d17cb6ae63552b75bff180f0942c1d8e0f5
torunse <etorunski@gmail.com>
2018-01-29 16:11:24 -0500

    set android version to 22

--- app/src/main/java/com/example/torunse/inclassexamples_18w/MainActivity.java
+++ app/src/main/java/com/example/torunse/inclassexamples_18w/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -13,21 +13,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //find the TextView and Button from the XML layout
-        TextView tv = (TextView)findViewById(R.id.textview);
-        Button btn1 = (Button)findViewById(R.id.button1);
+        Button helloButton = (Button)findViewById(R.id.hello_button);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+
+        EditText textField = (EditText)findViewById(R.id.editText);
+        helloButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 try {
-                    tv.setText("You clicked me");
+                    textField.setText("You clicked the button");
                 }
-                catch(NullPointerException npe)
+                catch (NullPointerException npe)
                 {
 
                 }
             }
         });
+
     }
 }
