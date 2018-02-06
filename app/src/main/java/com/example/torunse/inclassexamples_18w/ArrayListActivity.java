f5a15d928302b3ec9ef40d4a2c6170e1634f362f
torunse <etorunski@gmail.com>
2018-02-06 17:07:19 -0500

    starting ListAdapter

--- app/src/main/java/com/example/torunse/inclassexamples_18w/ArrayListActivity.java
+++ app/src/main/java/com/example/torunse/inclassexamples_18w/ArrayListActivity.java
@@ -3,13 +3,13 @@ package com.example.torunse.inclassexamples_18w;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class ListViewActivity extends Activity {
+public class ArrayListActivity extends Activity {
 
+    private String items[] = new String[]{ "A", "B" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_view);
-
+        setContentView(R.layout.activity_array_list);
     }
 }
