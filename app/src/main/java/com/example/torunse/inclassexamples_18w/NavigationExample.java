a615063523437776fe9e3216a6117d633a77eaee
torunse <etorunski@gmail.com>
2018-04-02 16:43:27 -0400

    sensors

--- app/src/main/java/com/example/torunse/inclassexamples_18w/NavigationExample.java
+++ app/src/main/java/com/example/torunse/inclassexamples_18w/NavigationExample.java
@@ -4,20 +4,19 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NavigationExample extends AppCompatActivity {
 
@@ -26,80 +25,81 @@ public class NavigationExample extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_example);
 
-        Toolbar toolbar = (Toolbar)findViewById(R.id.navigation_toolbar);
-        setSupportActionBar(toolbar);
+       // Toolbar tBar = (Toolbar)findViewById(R.id.toolbar_example);
+        //setSupportActionBar(tBar);
+
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu items for use in the action bar
+        // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.navigation_menu, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle presses on the action bar items
         switch (item.getItemId()) {
-            case R.id.menu_item_1:
-                Log.i("Menu 1 clicked:" , item.getTitle().toString());
+            case R.id.menu_item1:
                 //show a Toast
+                Toast.makeText(this, "Clicked item 1", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.menu_item_2:
-                Log.i("Menu 2 clicked:" , item.getTitle().toString());
-                LayoutInflater inflater = NavigationExample.this.getLayoutInflater();
-                View moreButtons = inflater.inflate(R.layout.dialog_layout, null);
-                CheckBox cb = (CheckBox)moreButtons.findViewById(R.id.checkBox);
-                Button btn = (Button)moreButtons.findViewById(R.id.button);
-                cb.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Log.i("CHeckbox:", "Clicked");
-                    }
-                });
-                btn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Log.i("Clicked", "Dialog button");
-                    }
-                });
-                AlertDialog.Builder builder = new AlertDialog.Builder(NavigationExample.this);
-                builder.setMessage("This is the message")
-                        .setNeutralButton("Maybe", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Log.i("Neutral","Neutral" );
+            case R.id.menu_item2:
+                LayoutInflater inflater = getLayoutInflater();
 
-                            }
-                        })
+                View extraStuff = inflater.inflate(R.layout.navigation_extra_items, null);
+                Button clickButton = (Button)extraStuff.findViewById(R.id.submit_button);
+                EditText input = (EditText)extraStuff.findViewById(R.id.extra_text);
+                clickButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String inText = input.getText().toString();
+                        Toast.makeText(NavigationExample.this, inText, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("This is the message")
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                Log.i("Positive","Positive" );
-                            }
-                         })
-                        .setView(moreButtons) // add the extra buttons to the dialog box
+                                // What to do on Accept
+                                }
+                        })
+                        .setView(extraStuff) //adds extra items to box
+
                         .setNegativeButton("No", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 // What to do on Cancel
-                                Log.i("Negative","Negative" );
-                                }
+
+                            }
+                        })
+                        .setNeutralButton("maybe", new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int id) {
+                                // What to do on Neutral button
+
+                            }
                         });
                 builder.create().show();
+
                 break;
-            case R.id.menu_item_3:
+            case R.id.menu_item3:
                 NotificationCompat.Builder mBuilder =
                         new NotificationCompat.Builder(this)
-                                .setSmallIcon(R.drawable.startup) //change this
+                                .setSmallIcon(R.drawable.worldwide)
                                 .setAutoCancel(true)
-                                .setContentTitle("My notification")//change this
-                                .setContentText("Hello World!");//change this
-                Intent resultIntent = new Intent(this, FragmentExample.class); //change this
-                PendingIntent resultPendingIntent = PendingIntent.getActivity( this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                                .setContentTitle("Any title string")
+                                .setContentText("Hello World!");
+                    Intent nextActivity = new Intent(this, SQLActivity.class);
+
+                PendingIntent resultPendingIntent = PendingIntent.getActivity( this, 0, nextActivity, PendingIntent.FLAG_UPDATE_CURRENT);
                 mBuilder.setContentIntent(resultPendingIntent);
-//Post the notification
-                int mNotificationId = 001;//change this
+
+                //post the notification:
+                int mNotificationId = 1;
                 NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 mNotifyMgr.notify(mNotificationId, mBuilder.build());
 
